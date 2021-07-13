c3b6df990f849bf88d13ad749ffaf2c08476c91c
naught <mark0naughton@gmail.com>
2021-07-13 23:09:32 +0100

    basic install

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+cp .config/i3/config ~/.config/i3/config
+cp .vimrc ~/.vimrc
+cp .config/rofi/config.rasi ~/.config/rofi/config.rasi
+cp .config/rofi/Naught-Dark.rasi ~/.config/rofi/Naught-Dark.rasi
+cp .config/i3status/config ~/.config/i3status/config
