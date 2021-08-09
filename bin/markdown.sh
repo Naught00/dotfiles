012aa7b0672873e6e42cfcb1a0f872f27f6d50eb
naught <mark0naughton@gmail.com>
2021-08-09 04:41:40 +0100

    updated markdown note taking script

--- bin/markdown.sh
+++ bin/markdown.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # A script that compiles my markdown notes into html and opens in luakit
+# Takes an argument in the form of a "file.md"
+# Can be easily intergrated into vim with the AsyncRun plugin. ":AsyncRun markdown.sh %"
 filename=$1
 name="${filename%.*}"
 cat $1 | marked > $name.html
