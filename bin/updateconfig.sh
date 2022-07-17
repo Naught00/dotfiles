49a5f94b23362fe9e54104446c7c94f0bf4a17b4
Mark Naughton <mark@marknaughton.com>
2022-07-17 15:34:40 +0100

    Updated config script

--- bin/updateconfig.sh
+++ bin/updateconfig.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp ~/.config/i3/config ~/git/dotfiles/.config/i3/config
-cp -r ~/.config/nvim/* ~/git/dotfiles/.config/nvim/
+shopt -s extglob
+cp -r ~/.config/nvim/!(pack) ~/git/dotfiles/.config/nvim/
 cp ~/.bashrc ~/git/dotfiles/.config/bash/.bashrc
 
