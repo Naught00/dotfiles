dbb60ce068a8fe74efb1e4357d1a0ab662cfb9a9
Mark Naughton <mark@marknaughton.com>
2022-09-25 22:16:20 +0100

    Add light mode script for tty

---
+++ bin/tty-light-mode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Sets the tty to light mode
+setterm --reverse on --store
+# Clear tty to reset colours
+clear
+
+# Sets a nice big font
+setfont /usr/share/kbd/consolefonts/latarcyrheb-sun32.psfu.gz
+
